f6e6e854b19aefe7d894a43c058f7ce8ec57af7b
Vilém Lipold <vilem.lipold@seznam.cz>
2024-06-16 19:52:39 +0200

    Dev server

--- webpack.config.common.mjs
+++ webpack.config.common.mjs
@@ -1,7 +1,7 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+export const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const config = {
   entry: './src/index.mjs',
--- webpack.config.dev.mjs
+++ webpack.config.dev.mjs
@@ -1,8 +1,20 @@
+import { join } from 'node:path';
 import { merge } from 'webpack-merge';
-import common from './webpack.config.common.mjs';
+import common, { __dirname } from './webpack.config.common.mjs';
 
 const devConfig = merge(common, {
   mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    port: 9000,
+    static: [
+      {
+        directory: join(__dirname, "./dist/"),
+        publicPath: '/', // nepovinné; defaultně je totiž '/'
+      },
+    ],
+  },
 });
 
 export default devConfig;
